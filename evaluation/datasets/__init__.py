7308801d5a30aeaa959f7d24dfc5d76e5df54b41
ankitrajmehta <ankitrajmehat16@gmail.com>
2026-05-12 21:01:13 +0545

    clean up

--- evaluation/datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Dataset module for evaluation framework."""
-
-from evaluation.datasets.base import BaseDataset
-from evaluation.datasets.locomo import LocomoDataset
-
-__all__ = ["BaseDataset", "LocomoDataset"]
--- evaluation/runners/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Evaluation runners module."""
-
-from evaluation.runners.base import BaseRunner
-from evaluation.runners.locomo import LocomoRunner
-
-__all__ = ["BaseRunner", "LocomoRunner"]
